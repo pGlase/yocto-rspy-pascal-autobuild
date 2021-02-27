3d4fbbd8b7ba3eb2d1c075682244f536bedb8945
Pascal Glase <killerspam@web.de>
2021-02-27 02:24:53 +0100

    updated script to dunfell

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 #only use it in the root
 
 #pull sources from git and make work dir
-git clone -b zeus git://git.yoctoproject.org/poky.git
-git clone -b zeus git://git.openembedded.org/meta-openembedded
-git clone -b zeus git://git.yoctoproject.org/meta-raspberrypi
-git clone -b zeus git://git.yoctoproject.org/meta-security
+git clone -b dunfell git://git.yoctoproject.org/poky.git
+git clone -b dunfell git://git.openembedded.org/meta-openembedded
+git clone -b dunfell git://git.yoctoproject.org/meta-raspberrypi
+git clone -b dunfell git://git.yoctoproject.org/meta-security
 git clone -b master https://github.com/pGlase/meta-pascal.git
 . poky/oe-init-build-env rpi-build
 #sourcing cds to $(pwd)/rpi-build!
